539f20fff8953f53806c71b3dd906ddca1d10db3
Myke Phillips <info@thefraternity.co.uk>
2018-09-12 07:24:03 +0100

    Created static data for tableView.

---
+++ Splitter/Images+Names+Colours.swift
@@ -0,0 +1,26 @@
+//
+//  Images+Names+Colours.swift
+//  Splitter
+//
+//  Created by Myke Phillips on 12/09/2018.
+//  Copyright © 2018 Myke Phillips. All rights reserved.
+//
+
+import UIKit
+
+let camera = UIImage(named: "1")!
+let city = UIImage(named: "2")!
+let buffalo = UIImage(named: "3")!
+let bouquet = UIImage(named: "4")!
+let stand = UIImage(named: "5")!
+let urban = UIImage(named: "6")!
+
+//Images
+let imageArray = [camera,city,buffalo,bouquet,stand,urban]
+
+//Names
+let namesArray = ["CAMERA","CITY","ANIMALS","FLOWERS","STANDS","URBAN",]
+
+
+//Colours
+let coloursArray = [#colorLiteral(red: 0.3098039329, green: 0.01568627544, blue: 0.1294117719, alpha: 1),#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1),#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1),#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1),#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1),#colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1),]
